63f8d89f24340eb3eaba970956f49fb7c461e16b
ckh <ckhtpe@gmail.com>
2018-11-16 17:11:09 +0800

    connection

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -7,6 +7,9 @@ class Register extends Component {
     constructor(props) {
         super(props);
     }
+    checkOut(e){
+        
+    }
     render() {
         return (
             <React.Fragment>
@@ -18,24 +21,26 @@ class Register extends Component {
                         </div>
                         <div class="formPattern fadeInRight" style={{ width: '300px' }}>
                             <h3 className='pb-4 text-center'>註冊會員</h3>
-                            <form>
+                            <form id='registerFrom' action='http://localhost:3000/api/members' method='post'>
                                 <div class="form-group d-flex justify-content-between py-1">
                                     <label>暱稱</label>
-                                    <input type="email" class="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入暱稱" />
+                                    <input id='nickName' name='nick_name' type="text" class="form-control w-75"  aria-describedby="emailHelp" placeholder="請輸入暱稱" />
                                 </div>
                                 <div class="form-group d-flex justify-content-between py-1">
                                     <label>Email</label>
-                                    <input type="email" class="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入信箱" />
+                                    <input id='eamil' name='email' type="email" class="form-control w-75"  aria-describedby="emailHelp" placeholder="請輸入信箱" />
                                 </div>
                                 <div class="form-group d-flex justify-content-between py-1">
                                     <label>密碼</label>
-                                    <input type="email" class="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入密碼" />
+                                    <input id='pw1' name='password' type="password" class="form-control w-75"  aria-describedby="emailHelp" placeholder="請輸入密碼" />
                                 </div>
                                 <div class="form-group d-flex justify-content-between py-1">
                                     <label>密碼確認</label>
-                                    <input type="password" class="form-control w-75" id="exampleInputPassword1" placeholder="請確認密碼" />
+                                    <input id='pw2' type="password" class="form-control w-75"  placeholder="請確認密碼" />
                                 </div>
-                                <Link to='/registerSuccessful'><button type="submit" class="btn btn-primary w-100 py-2 btn1">註冊</button></Link>
+                                {/* <Link to='/registerSuccessful'> */}
+                                <button id='registerBtn' onClick={this.checkOut} type="submit" class="btn btn-primary w-100 py-2 btn1">註冊</button>
+                                {/* </Link> */}
                                 <small className="text-center">繼續進行代表你同意<span className="text-primary">服務條款</span></small>
                                 <hr />
                                 <Link to='/memberCenter/basicInfo'><button type="submit" class="btn btn-primary py-2 w-100">登入</button></Link>
@@ -47,7 +52,7 @@ class Register extends Component {
         );
     }
     componentDidMount() {
-
+       
     }
 }
 
