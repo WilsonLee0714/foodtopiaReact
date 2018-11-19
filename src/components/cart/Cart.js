848bf33b1a2d339b10a6ea1e6e795dbe31ea5c7c
Wilson Lee <wilsonlee0714@gmail.com>
2018-11-20 01:13:52 +0800

    bug

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,64 +1,74 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import "./Cart.scss";
+import Products from "./Cart.json";
 
 class Cart extends Component {
   constructor(props) {
     super(props);
     this.initState = {
+      id: "",
       name: "",
       note: "",
+      num: "",
       img: "",
-      price: "",
-      id: ""
+      perPrice: ""
   }
 
   this.state = {
-      products: [],
+      products: [Products],
       product: this.initState,
+      amout:'',
       type: 'add'
   }
+  // this.price = (this.state.product.num)*(this.state.product.perPrice);
   }
   render() {
+    var productImg = "require(.${product.img})"
+    
     return (
       <React.Fragment>
         <div id='cart' className='cart'>
           <div className='title'>購物車</div>
           <div id='products'>
+          {this.state.products.map(product =>
             <div className='row my-2'>
               <div className='col-5 productImg'>
-                <img src={require('./images/1.jpeg')}/>
+                <img src={productImg}/>
               </div>
               <div className='col-7 productName'>
-                <span>精選土虱</span><br/>
-                <span>來自新鮮養殖</span>
+                <span>{product.name}</span><br/>
+                <span>{product.note}</span>
               </div>
               <div className='col-5 productNum'>
                 <span className='btnMinus'>
                   <i class="fas fa-minus"></i>
                 </span>
-                <span>1</span>
+                <span>{product.num}</span>
                 <span className='btnPlus'>
                   <i className="fas fa-plus"></i>
                 </span>
               </div>
               <div className='col-7 productPrice'>
-                <span>NT$ 79</span>
+                <span>NT$ </span><span>{this.price}</span>
               </div>
             </div>
+          )}
           </div>
           <div className='container'>
           <hr className='line1'/>
           </div>
           <div className='totalPrice'>
-          <span>總計: NT$ 1,573</span>
+          <span>總計: NT$</span><span>1,573</span>
           </div>
-          <button onClick='' type="submit" class="btn btnCheckOut d-flex px-5">開始結帳</button>
+          <Link to='/order'><button onClick='' type="submit" class="btn btnCheckOut d-flex px-5">開始結帳</button></Link>
         </div>
       </React.Fragment>
     );
   }
-  componentDidMount() {}
+  componentDidMount() {
+    // console.log(this.state.product);
+  }
 }
 
 export default Cart;
