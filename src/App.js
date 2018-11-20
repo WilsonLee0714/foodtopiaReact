952d6a3d802f8d5b9806c7a4f82fa26fed240055
ckh <ckhtpe@gmail.com>
2018-11-20 15:57:30 +0800

    link and manager interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Nav from "./components/nav/Nav.js";
@@ -14,23 +14,22 @@ import MyService from './components/myService/MyService.js';
 import Footer from './components/footer/Footer.js';
 import HomePage from './components/homePage/HomePage.js';
 
-
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <Nav />
-          <Route path="/homePage" component={HomePage} />
-          <Route path="/register" component={Register} />
-          <Route path="/registerSuccessful" component={RegisterSuccessful} />
+          <Nav/>
+          <Route path="/homePage" component={HomePage}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/registerSuccessful" component={RegisterSuccessful}/>
           <div className="container d-flex">
-            <Route path="/memberCenter" component={MemberCenter} />
-            <Route path="/memberCenter/BasicInfo" component={BasicInfo} />
-            <Route path="/memberCenter/subscription" component={Subscription} />
-            <Route path="/memberCenter/favorite" component={Favorite} />
-            <Route path="/memberCenter/myOrder" component={MyOrder} />
-            <Route path="/memberCenter/myService" component={MyService} />
+            <Route path="/memberCenter" component={MemberCenter}/>
+            <Route path="/memberCenter/BasicInfo" component={BasicInfo}/>
+            <Route path="/memberCenter/subscription" component={Subscription}/>
+            <Route path="/memberCenter/favorite" component={Favorite}/>
+            <Route path="/memberCenter/myOrder" component={MyOrder}/>
+            <Route path="/memberCenter/myService" component={MyService}/>
           </div>
           <Footer/>
         </React.Fragment>
