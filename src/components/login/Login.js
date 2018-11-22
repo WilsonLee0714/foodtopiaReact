0bcdc7720f353ad1967a06dffb47a184e315bd35
ckh <ckhtpe@gmail.com>
2018-11-22 12:52:02 +0800

    css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,48 +7,60 @@ class Login extends Component {
     constructor(props) {
         super(props);
     }
-    checkOut(e){
+    checkOut(e) {
+        e.preventDefault();
         let email = document.getElementById('email');
         let pw1 = document.getElementById('pw1');
-        if(email.value == '' || pw1.value ==''){
+        let registerFrom = document.getElementById('registerFrom');
+        if (email.value == '' || pw1.value == '') {
             alert('信箱或密碼不正確')
-        } 
+        } else {
+            registerFrom.submit();
+        }
     }
     render() {
         return (
             <React.Fragment>
-                
-                <div className='container'>
-                    <div className='d-flex justify-content-center align-items-center mt-3 mb-5 login flex-md-row flex-column-reverse'>
-                        {/* <div className='d-flex fadeInLeft'>
+                <div className='wrapLogin'>
+                    <img className='bgtable' src={require('./images/tables.jpg')} />
+                    <div className='container'>
+                        <div className='d-flex justify-content-center align-items-center mt-3 mb-5 login flex-md-row flex-column-reverse'>
+                            {/* <div className='d-flex fadeInLeft'>
                             <img className='' src={require('./images/steakPlated.png')} />
                         </div> */}
-                        <div class="formPattern fadeIn" style={{ width: '300px' }}>
-                            <form id='registerFrom' action='http://localhost:3000/users/login' method='post'>
-                            <h3 className='pb-4 text-center'>會員登入</h3>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>Email</label>
-                                    <input id='email' name='email' type="email" class="form-control w-75"  aria-describedby="emailHelp" placeholder="請輸入信箱" />
-                                </div>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>密碼</label>
-                                    <input id='pw1' name='password' type="password" class="form-control w-75"  aria-describedby="emailHelp" placeholder="請輸入密碼" />
-                                </div>
-                                {/* <Link to='/registerSuccessful'> */}
-                                <button id='registerBtn' onClick={this.checkOut} type="submit" class="btn btn-primary w-100 py-2 btn1 btn-sm">登入</button>
-                                {/* </Link> */}
-                                <small className="text-center"><span className="text-primary">忘記密碼</span></small>
-                                <hr />
-                                <Link to='/register'><button type="submit" class="btn btn-primary btn-sm py-2 w-100">註冊</button></Link>
-                            </form>
+                            <div class="formPattern fadeIn" style={{ width: '300px' }}>
+                                <form id='registerFrom' action='http://localhost:3000/users/login' method='post'>
+                                    <h3 className='pb-4 text-center'>會員登入</h3>
+                                    <div class="form-group  py-1">
+                                        <label>Email :</label>
+                                        <input id='email' name='email' type="email" class="w-100 form-control w-75" aria-describedby="emailHelp" placeholder="請輸入信箱" />
+                                    </div>
+                                    <div class="form-group  py-1">
+                                        <label>密碼 :</label>
+                                        <input id='pw1' name='password' type="password" class="w-100 form-control w-75" aria-describedby="emailHelp" placeholder="請輸入密碼" />
+                                    </div>
+                                    {/* <Link to='/registerSuccessful'> */}
+                                    <button id='loginBtn' onClick={this.checkOut} type="submit" class="btn btn-primary w-100 py-2 btn1 btn-sm">登入</button>
+                                    {/* </Link> */}
+                                    {/* <small className="text-center"><span className="text-primary">忘記密碼</span></small> */}
+                                    <hr />
+                                    <div className=''>
+                                        <small className="text-center py-3"><span className="text-dark">還不是會員嗎?</span></small>
+                                        <br/>
+                                        <Link to='/register'><button type="submit" class="btn btn-primary btn-sm py-2 w-100">立即註冊</button></Link>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
             </React.Fragment>
         );
     }
     componentDidMount() {
-       
+        window.scrollTo(0, 0);
     }
 }
 
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -9,6 +9,14 @@ class Register extends Component {
     }
     checkOut(e) {
         e.preventDefault();
+        function IsEmail(email) {
+            var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if (!regex.test(email)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
         var nickName = document.getElementById('nickName');
         var email = document.getElementById('email');
         var pw1 = document.getElementById('pw1');
@@ -26,45 +34,51 @@ class Register extends Component {
             email.value = '';
             pw1.value = '';
             pw2.value = '';
+        } else if (!IsEmail(email.value)) {
+            alert('信箱格式不正確')
+            console.log(IsEmail(email.value))
         } else {
+            console.log(IsEmail(email.value))
             registerFrom.submit();
         }
     }
     render() {
         return (
             <React.Fragment>
-
-                <div className='container'>
-                    <div className='d-flex justify-content-center align-items-center mt-3 mb-5 register flex-md-row flex-column-reverse'>
-                        <div className='d-flex fadeInLeft'>
-                            <img className='' src={require('./images/steakPlated.png')} />
-                        </div>
-                        <div class="formPattern fadeInRight" style={{ width: '300px' }}>
-                            <h3 className='pb-4 text-center'>會員註冊</h3>
-                            <form id='registerFrom' action='http://localhost:3000/api/members' method='post'>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>暱稱</label>
-                                    <input id='nickName' name='nick_name' type="text" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入暱稱" />
-                                </div>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>Email</label>
-                                    <input id='email' name='email' type="email" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入信箱" />
-                                </div>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>密碼</label>
-                                    <input id='pw1' name='password' type="password" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入密碼" />
-                                </div>
-                                <div class="form-group d-flex justify-content-between py-1">
-                                    <label>密碼確認</label>
-                                    <input id='pw2' type="password" class="form-control w-75" placeholder="請確認密碼" />
-                                </div>
-                                {/* <Link to='/registerSuccessful'> */}
-                                <button id='registerBtn' onClick={this.checkOut} type="submit" class="btn btn-sm btn-primary w-100 py-2 btn1">註冊</button>
-                                {/* </Link> */}
-                                <small className="text-center">繼續進行代表你同意<span className="text-primary">服務條款</span></small>
-                                <hr />
-                                <Link to='/login'><button type="submit" class="btn btn-sm btn-primary py-2 w-100">登入</button></Link>
-                            </form>
+                <div className='wrapRe'>
+                    <img className='tree' src={require('./images/table2.jpg')} />
+                    <div className='container'>
+                        <div className='d-flex justify-content-center align-items-center mt-3 mb-5 register flex-md-row flex-column-reverse'>
+                            <div className='d-flex fadeInLeft'>
+                                <img className='' src={require('./images/steakPlated.png')} />
+                            </div>
+                            <div class="formPattern fadeInRight" style={{ width: '300px' }}>
+                                <form id='registerFrom' action='http://localhost:3000/api/members' method='post'>
+                                <h3 className='pb-4 text-center'>會員註冊</h3>
+                                    <div class="form-group d-flex justify-content-between py-1">
+                                        <label>暱稱</label>
+                                        <input id='nickName' name='nick_name' type="text" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入暱稱" />
+                                    </div>
+                                    <div class="form-group d-flex justify-content-between py-1">
+                                        <label>Email</label>
+                                        <input id='email' name='email' type="email" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入信箱" />
+                                    </div>
+                                    <div class="form-group d-flex justify-content-between py-1">
+                                        <label>密碼</label>
+                                        <input id='pw1' name='password' type="password" class="form-control w-75" aria-describedby="emailHelp" placeholder="請輸入密碼" />
+                                    </div>
+                                    <div class="form-group d-flex justify-content-between py-1">
+                                        <label>密碼確認</label>
+                                        <input id='pw2' type="password" class="form-control w-75" placeholder="請確認密碼" />
+                                    </div>
+                                    {/* <Link to='/registerSuccessful'> */}
+                                    <button id='registerBtn' onClick={this.checkOut} type="submit" class="btn btn-sm btn-primary w-100 py-2 btn1">註冊</button>
+                                    {/* </Link> */}
+                                    <small className="text-center">繼續進行代表你同意<span className="text-primary">服務條款</span></small>
+                                    <hr />
+                                    <Link to='/login'><button type="submit" class="btn btn-sm btn-primary py-2 w-100">登入</button></Link>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -72,7 +86,7 @@ class Register extends Component {
         );
     }
     componentDidMount() {
-
+        window.scrollTo(0,0);
     }
 }
 
