7e4e26e0e8a83d890d6f035201e3d6fe00e2c3d3
ckh <ckhtpe@gmail.com>
2018-11-26 15:17:34 +0800

    put ok

--- src/components/basicInfo/BasicInfo.js
+++ src/components/basicInfo/BasicInfo.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 class BasicInfo extends Component {
     constructor(prop) {
         super(prop);
-
-        this.state={
-            nickname:'',
-            name:'',
-            email:'',
+        this.state = {
+            nickname: '',
+            name: '',
+            email: '',
+            sid: '',
         }
     }
-    componentDidMount() {
+    componentDidMount = () => {
         var name = document.getElementById('name');
         var email = document.getElementById('email');
         var nickname = document.getElementById('nickname');
@@ -22,19 +22,39 @@ class BasicInfo extends Component {
         }).then(function (res) {
             console.log(res);
             return res.json();
-        }).then(function (a) {
-            email.value = a.email;
-            name.value = a.name;
-            nickname.value = a.nickname;
+        }).then((a) => {
+            this.setState({ nickname: a.nickname });
+            this.setState({ sid: a.sid });
+            this.setState({ email: a.email });
+            this.setState({ name: a.name });
         }).catch(function (err) {
             console.log(err);
             //alert(err);
         })
     }
+
+    handleChange = (event) => {
+        this.setState({ nickname: event.target.value });
+    }
+    // handleClick = (event) => {
+    //     // alert(this.state.nickname);
+    //     event.preventDefault();
+    //     fetch('http://localhost:3000/session/info', {
+    //         method: 'PUT',
+    //         // credentials: 'include',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             nick_name: this.state.nickname,
+    //             sid: this.state.sid
+    //         })
+    //     });
+    // }
     render() {
         return (
             <React.Fragment>
-                <div className='basicWrap'>
+                <div className='basicWrap' >
                     <div className='profileImg mx-auto my-3'>
                         <img src={require('./images/profile.png')} />
                     </div>
@@ -43,16 +63,17 @@ class BasicInfo extends Component {
                         <button className='btn btn-btn-secondary btn-sm' type='button'>選擇檔案</button>
                     </div>
                     <div className='infoWrap m-5 p-3'>
-                        <form>
+                        <form action='http://localhost:3000/session/info' method='post'>
                             <h4 className='infoTitle p-3'>基本資料</h4>
                             <table className=''>
                                 <tr>
                                     <td><span className='text-danger'>*</span>顯示暱稱</td>
-                                    <td><input id='nickname' className='' size='50' type='text'></input></td>
+                                    <td><input id='nickname' name='nick_name' value={this.state.nickname} onChange={this.handleChange} className='' size='50' type='text'></input></td>
                                 </tr>
                                 <tr>
                                     <td >帳號</td>
                                     <td><input className='' size='50' type='text'></input></td>
+                                    <td><input style={{display:'none'}} name='sid' value={this.state.sid} className='' size='50' type='text'></input></td>
                                 </tr>
                                 <tr>
                                     <td>密碼</td>
@@ -64,7 +85,7 @@ class BasicInfo extends Component {
                                 </tr>
                                 <tr>
                                     <td>Email</td>
-                                    <td><input id='email' className='' size='50' type='text'></input></td>
+                                    <td><input id='email' name='email' value={this.state.email} className='' size='50' type='text'></input></td>
                                 </tr>
                                 <tr>
                                     <td>性別</td>
@@ -104,7 +125,7 @@ class BasicInfo extends Component {
                                 </tr>
                             </table>
                             <div className='text-center my-3'>
-                                <button className='btn btn-success w-25'>儲存變更</button>
+                                <button type='submit' onClick={this.handleClick} className='btn btn-success w-25'>儲存變更</button>
                             </div>
                         </form>
                     </div>
