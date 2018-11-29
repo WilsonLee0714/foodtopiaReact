443c9f6c9eb82c2feb84fc23b8996d5d0d34ded2
ckh <ckhtpe@gmail.com>
2018-11-29 12:26:34 +0800

    homepage css animation

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,73 +1,69 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Footer.scss';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 class Footer extends Component {
-    constructor(prop) {
-        super(prop)
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <footer>
-                    <div className='container d-flex justify-content-between py-5'>
-                        <div className='d-flex'>
-                            <div className='pr-4'>
-                                <ul>
-                                    <li>首頁</li>
-                                    <li>生鮮食材</li>
-                                    <li>美味食譜</li>
-                                    <li>關於我們</li>
-                                    <li>會員中心</li>
-                                    <li>購物清單</li>
-                                </ul>
-                            </div>
-                            <div className='pr-4'>
-                                <ul>
-                                    <li>首頁</li>
-                                    <li>生鮮食材</li>
-                                    <li>美味食譜</li>
-                                    <li>關於我們</li>
-                                    <li>會員中心</li>
-                                    <li>購物清單</li>
-                                </ul>
-                            </div>
-                            <div>
-                                <ul>
-                                    <li>首頁</li>
-                                    <li>生鮮食材</li>
-                                    <li>美味食譜</li>
-                                    <li>關於我們</li>
-                                    <li>會員中心</li>
-                                    <li>購物清單</li>
-                                </ul>
-                            </div>
+  constructor(prop) {
+    super(prop)
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <footer>
+          <div className='container d-flex justify-content-between py-5'>
+            <div className='d-flex'>
+              <div className='pr-4'>
+                <ul>
+                  <li>首頁</li>
+                  <li>生鮮食材</li>
+                  <li>美味食譜</li>
+                  <li>關於我們</li>
+                  <li>會員中心</li>
+                </ul>
+              </div>
+              <div className='pr-4'>
+                <ul>
+                  <li>會員中心</li>
+                  <li>基本資料</li>
+                  <li>訂閱通知</li>
+                  <li>我的訂單</li>
+                  <li>客服系統</li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>購物車</li>
+                  <li>購物清單</li>
+                  <li>客服系統</li>
+                  <li>部落格</li>
+                </ul>
+              </div>
 
-                        </div>
-                        <div className='d-flex'>
-                            <div className='pr-5 flex-1'>
-                                社群
-                        </div>
-                            <div className='pl-5 flex-1'>
-                                <div>
-                                    語言<br/>
-                            <select>
-                                        <option>繁體中文</option>
-                                    </select>
-                                </div>
-                                <div>
-                                購物所在地<br/>
-                            <select>
-                                    <option>台灣</option>
-                                </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </React.Fragment>
-        )
-    }
+            </div>
+            <div className='d-flex social'>
+              <div className='pr-1 flex-1'>
+                <img className='ss' src={require('./images/i13.png')}/>
+              </div>
+              <div className='pl-5 flex-1'>
+                <div>
+                  語言<br/>
+                  <select>
+                    <option>繁體中文</option>
+                  </select>
+                </div>
+                <div>
+                  購物所在地<br/>
+                  <select>
+                    <option>台灣</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Footer;
--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './HomePage.scss';
 import { Link } from 'react-router-dom';
-import $ from 'jquery'; 
+import $ from 'jquery';
 
 class HomePage extends Component {
     constructor(prop) {
@@ -9,33 +9,73 @@ class HomePage extends Component {
     }
     componentDidMount() {
         window.scrollTo(0, 0);
+        setTimeout(function () {
+            $('.i1').css('opacity', '1').css('transform', 'translateY(0)');
+        }, 300)
+        setTimeout(function () {
+            $('.i2').css('opacity', '1').css('transform', 'translateY(0)');
+        }, 600)
+        setTimeout(function () {
+            $('.i3').css('opacity', '1').css('transform', 'translateY(0)');
+        }, 900)
+        $(window).scroll(function () {
+            let scrollNow = $(this).scrollTop();
+            console.log(scrollNow);
+            let scrollHeight = $(window).height();
+            console.log(scrollHeight);
+            if (scrollNow >= scrollHeight / 2) {
+                // $('.mainWrap').css('top', scrollHeight + 'px').css('opacity','1')
+                $('.secondWrap').css('opacity', '1').css('transform', 'translateX(0)');
+            }
+            if (scrollNow >= scrollHeight) {
+                // $('.mainWrap').css('top', scrollHeight + 'px').css('opacity','1')
+                $('.mainWrap').css('opacity', '1').css('transform', 'translateX(0)')
+            }
+            if (scrollNow >= scrollHeight * 1.5) {
+                // $('.mainWrap').css('top', scrollHeight + 'px').css('opacity','1')
+                $('.thirdWrap').css('opacity', '1').css('transform', 'translateX(0)')
+            }
+            //關於立方掉落動畫開始
+            // var x = $('.distance').offset().top
+            // console.log(scrollNow)
+            // if (scrollNow > x / 2 ) {
+            //     console.log(x)
+            //     $('.wrapper').css('transform', 'translateY(0)')
+            // }
+            //關於立方掉落動畫結束
+        })
     }
     smoothscroll() {
-            $("html, body").animate({ scrollTop: 0 }, "slow");
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     }
     render() {
         return (
             <React.Fragment>
                 <header>
+                    <div className='foodTitle'>
+                        <h2 className='rubberBand'>依照食譜</h2>
+                        <h2 className='rubberBand2'>輕鬆買齊所有食材</h2>
+                        <h3 className='rubberBand3'>Foodtopia 服務最便利的線上超市</h3>
+                    </div>
                     <div className='firstMenu d-flex justify-content-center'>
-                        <img src={require('./images/Group125.png')} />
-                        <img className='mx-5' src={require('./images/Group127.png')} />
-                        <img src={require('./images/Group777.png')} />
+                        <img className='i i1' src={require('./images/Group125.png')} />
+                        <img className='mx-5 i i2' src={require('./images/Group127.png')} />
+                        <img className='i i3' src={require('./images/Group777.png')} />
                     </div>
                 </header>
-                <div className='home'>
+                <div className='home '>
                     <div>
                         <h3 className='text-center py-5'>一鍵採買食材</h3>
                     </div>
-                    <div className='secondMenu d-flex justify-content-center py-4'>
+                    <div className='secondMenu d-flex justify-content-center py-4 secondWrap'>
                         <img className='mx-5' src={require('./images/Group72.png')} />
                         <img className='mx-5' src={require('./images/Group70.png')} />
                         <img className='mx-5' src={require('./images/Group71.png')} />
                     </div>
-                    <div className='my-5 py-4 text-center'>
+                    <div className='my-5 py-4 text-center mainWrap'>
                         <img className='mx-5' style={{ width: '1000px' }} src={require('./images/Web111.png')} />
                     </div>
-                    <div className='sFotter d-flex justify-content-center align-items-center'>
+                    <div className='sFotter d-flex justify-content-center align-items-center thirdWrap'>
                         <div className='smallBox1 '>
                             <div className='text-center pt-4 pb-3'>最齊全的食譜種類<br />最便捷的食材採買服務</div>
                             <div className='text-center'>
@@ -45,7 +85,7 @@ class HomePage extends Component {
                     </div>
                 </div>
                 <div className='toTop' onClick={this.smoothscroll}>
-                    <img src={require('./images/toTop.png')}/>
+                    <img src={require('./images/toTop.png')} />
                 </div>
             </React.Fragment>
         )
