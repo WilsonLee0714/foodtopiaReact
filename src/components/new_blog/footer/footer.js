9dd2782377937c3e5b7feed92564b44207fb98af
John Doe <johndoe@example.com>
2018-12-11 21:10:02 +0800

    ming/12-11/21:00

--- src/components/new_blog/footer/footer.js
+++ src/components/new_blog/footer/footer.js
@@ -31,14 +31,12 @@ class Footer extends Component {
                     <h2>個人社群↓</h2>
                     <div className="blog_footer_icon container d-flex">
                         {this.props.communitys.map(community =>
-                        
-                            <div key={community.id} className="footer_icon d-flex justify-content-between">
-                                <a className="link_black " id="alink1" href={(`${community.facebook}`)}><i className="icon fab fa-facebook col-2"></i></a>
-                                <a className="link_black " id="alink2" href={(`${community.instagram}`)}><i className="icon fab fa-instagram col-2"></i></a>
-                                <a className="link_black " id="alink3" href={(`${community.google_plus}`)}><i className="icon fab fa-google-plus-g col-2"></i></a>
-                                <a className="link_black " id="alink4" href={(`${community.youtube}`)}><i className="icon fab fa-youtube col-2"></i></a>
+                            <div key={community.id} className="footer_icon d-flex row">
+                                <a className="link_black col-3" id="alink1" href={(`${community.facebook}`)}><i className="icon fab fa-facebook col-2"></i></a>
+                                <a className="link_black col-3" id="alink2" href={(`${community.instagram}`)}><i className="icon fab fa-instagram col-2"></i></a>
+                                <a className="link_black col-3" id="alink3" href={(`${community.google_plus}`)}><i className="icon fab fa-google-plus-g col-2"></i></a>
+                                <a className="link_black col-3" id="alink4" href={(`${community.youtube}`)}><i className="icon fab fa-youtube col-2"></i></a>
                             </div>
-                            
                         )}
                     </div>
                 </div>
