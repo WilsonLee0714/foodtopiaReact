1538ed04a5536d55d15d8a60a7bb53e9793ff68d
yvonne <yvonne250003@gmail.com>
2018-12-13 11:45:16 +0800

    1213 yvn modify ingredient page

--- src/components/igr_homepage/ingredient_active/Ingredient_active.js
+++ src/components/igr_homepage/ingredient_active/Ingredient_active.js
@@ -11,7 +11,7 @@ class Ingredient_active extends Component {
             <h2>推薦食譜</h2>
             <p>Foodtopia 每日提供多元的推薦食譜，豐富你的飲食生活</p>
             <Link  className="link_sec6" to="/recipe_page">
-              <span>立即取得最新推薦</span>
+              <span className="cta_btn">立即取得最新推薦</span>
             </Link>
           </div>
           <div className="sec6_right_img"></div>
